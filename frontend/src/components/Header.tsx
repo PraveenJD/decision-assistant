8a1e7c206451723e8661b55e9a2c6773d2887997
praveen <jd.praveen001@gmail.com>
2025-04-29 13:15:01 +0530

    updated title

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="h-20 border-b border-gray-200 shadow flex items-center justify-between px-10">
       <img src={icon} alt="novartis" className="w-64" />
-      <h1 className="text-3xl font-bold">Sample Text</h1>
+      <h1 className="text-3xl font-bold">Personalised Decision Assistant</h1>
     </div>
   );
 };
